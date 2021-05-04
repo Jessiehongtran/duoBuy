6a031fa411640c65af53e68bbd51fb78d7ff7455
Hong Tran <jessietran@fb.com>
2021-05-03 21:17:03 -0700

    assembled GET and POST products into frontend

--- index.js
+++ index.js
@@ -1,17 +1,5 @@
 const API_URL = 'http://localhost:4001'
 
-const products = [
-    {
-        uid: Date.now(),
-        product_image: "https://az870929.vo.msecnd.net/cdn/media/data/default/product-large.ashx",
-        product_name: "abc",
-        product_price: 22,
-        current_price: 11,
-        cobuyers_num: 2,
-        actual_cobuyers: 1
-    }
-]
-
 const productContainer = document.getElementById("products")
 const newProduct = document.getElementById("newProduct")
 const walletBudget = document.getElementsByClassName("money")[0]
@@ -19,7 +7,7 @@ const howItWorks = document.getElementsByClassName("how-it-works")[0]
 
 let actualCobuyers = 1
 
-function displayProducts(){
+function displayProducts(products){
     newProduct.style.display = 'none'
     
     if (productContainer.firstChild){
@@ -66,7 +54,7 @@ function displayProducts(){
         img.src = products[i].product_image
         name.innerHTML = products[i].product_name
         maxPrice.innerHTML = products[i].current_price
-        minPrice.innerHTML = Math.floor(products[i].product_price/products[i].cobuyers_num)
+        minPrice.innerHTML = Math.round(products[i].product_price/products[i].cobuyers_total)
         if (parseInt(maxPrice.innerHTML) < parseInt(minPrice.innerHTML)){
             minPriceContainer.style.display = 'none'
         }
@@ -99,8 +87,7 @@ function displayProducts(){
 const cloudName = 'demo'
 const unsignedUploadPreset = 'doc_codepen_example';
 
-function uploadFile(file){
-    console.log('file', file)
+function uploadFile(file, newProduct){
     let url = `https://api.cloudinary.com/v1_1/${cloudName}/image/upload`;
     let xhr = new XMLHttpRequest();
     let fd = new FormData();
@@ -114,7 +101,8 @@ function uploadFile(file){
             console.log('url', url)
             let tokens = url.split('/');
             tokens.splice(-2, 0, 'w_150,c_scale');
-            products[products.length - 1].product_image = url;
+            newProduct.product_image = url;
+            addProductToServer(newProduct)
         }
     }
     fd.append('upload_preset', unsignedUploadPreset);
@@ -131,28 +119,24 @@ function addProduct(){
     const expectedCouyers = document.getElementById("cobuyer").value
     const productImg = document.getElementById("productImg").value
     const newProduct = {
-        uid: Date.now(),
         product_image: "",
         product_name: name,
         product_price: parseInt(price),
         current_price: Math.floor(parseInt(price)/2),
-        cobuyers_num: parseInt(expectedCouyers),
-        group_code: Math.random().toString(36).slice(-4),
+        cobuyers_total: parseInt(expectedCouyers),
+        hostId: 1,
         host_code: Math.random().toString(36).slice(-3),
         actual_cobuyers: 1
     }
 
-    //add product
-    products.push(newProduct)
-
     if (productImg){
-        uploadFile(productImg)
+        uploadFile(productImg, newProduct)
     } else {
-        products[products.length - 1].product_image = "https://az870929.vo.msecnd.net/cdn/media/data/default/product-large.ashx"
+        newProduct.product_image = "https://az870929.vo.msecnd.net/cdn/media/data/default/product-large.ashx"
+        addProductToServer(newProduct)
     }
 
-    displayProducts()
-    
+    getProductsFromServer()
 
 }
 
@@ -193,46 +177,8 @@ function handleJoinBuying(i){
     let money = document.getElementsByClassName("money")[0]
     money.innerHTML = parseInt(money.innerHTML) - products[i].current_price
     //reimburse for existing cobuyers an amount = price/cur_cobuyers - price/(cur_cobuyers + 1)
-    
-
-    //questions: who will hold this contributing money till the date to buy stuff
-    //what's the motivation to pay first (to make the price cheaper?), the one who pays first should be paying less
-    //$30: 1st per
-    //$15: 2nd per, -15 for 1st per
-    //$10: 3rd per, -5 for 1st per -5 for 2nd per
-    //$7.5: 4th per, -2.5 for 1st per, -2.5 for 2nd per, -2.5 for 3rd per
-    //there is also max cobuyers they can set so that they don't have to pay too much, once max-cobuyer is met or min-pay is met or expiration date or everyone decided to buy, 
-    //the one who initiated will buy and share the thing to the group, how to ensure he will buy and share? also he does not collect money, how about we collect money and buy then distribute to them
-    //but then they need to trust us? and how are we going to distribute the good to each of them
 }
 
-
-//100, 3 cobuyers >> total 4 >> 25 each ideally
-
-//min: 25, max: 50
-//host pays $100
-//1st buy: pay $50, all got reimbursed price/cur_cobuyers - price/(cur_cobuyers + 1)
-//min: 25, max: 33
-//2nd buy: pay $33, all got reimbursed price/cur_cobuyers - price/(cur_cobuyers + 1)
-//min: 0, max: 25
-//3rd buy: pay $25, all got reimbursed price/cur_cobuyers - price/(cur_cobuyers + 1) 
-
-
-
-
-//coins assemble
-//what happens if they have enough money? who is owning all money at that time and who is going to buy
-
-//who will buy: give them one time access into the fund? how do we make sure it's an agreement of all cobuyers (each contribute a part of the code, the code is combined of group code and individual code, after a certain time if the code is not decoded, money will be returned to each of them? do we take care of whether they actually share using it after buying?
-// const password = 'abc123'
-// const rounds = 10
-// bcrypt.hash(password, rounds, (err, hash) => {
-//     if (err){
-//         console.error(err)
-//         return 
-//     }
-//     console.log(hash)
-// })
 
 
 function addProductToServer(newProduct){
@@ -245,26 +191,26 @@ function addProductToServer(newProduct){
     xhr.onreadystatechange = function (e){
         if (xhr.readyState == 4 && xhr.status == 200){
             let response = JSON.parse(xhr.responseText);
-            console.log('response in post', response)
+            getProductsFromServer()
         }
-        console.log(xhr.responseText)
     }
     xhr.send(JSON.stringify(newProduct)) 
-    xhr.send(newProduct)
 }
-
-addProductToServer({
-    product_name: 'CANON 4 camera',
-	product_image: '',
-	product_price: 123,
-	current_price: 62,
-	cobuyers_total: 2,
-	actual_cobuyers: 1,
-	host_code: 'CD',
-	hostId: 1
-})
 
 
 function getProductsFromServer(){
-
+    let url =  `${API_URL}/product`
+    let xhr = new XMLHttpRequest();
+    xhr.open('GET', url, true);
+    xhr.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
+    xhr.onreadystatechange = function (e){
+        if (xhr.readyState == 4 && xhr.status == 200){
+            let response = JSON.parse(xhr.responseText);
+            console.log('products', response)
+            displayProducts(response)
+        }
+    
+    }
+    xhr.send()
 }
+
