5c09b98acf80210e79e1d3583e60f027a647450e
Hong Tran <jessietran@fb.com>
2021-08-07 09:34:58 -0700

    added members

--- cobuyerGroup.js
+++ cobuyerGroup.js
@@ -6,16 +6,16 @@ const productImage = document.getElementsByClassName("product-image")[0]
 const productName = document.getElementsByClassName("product-name")[0]
 const minPrice = document.getElementsByClassName("min-price")[0]
 const maxPrice = document.getElementsByClassName("max-price")[0]
+const cobuyerContainer = document.getElementsByClassName("cobuyers")[0]
 
 getCobuyersOfAProduct()
 getProductById()
-
-let cobuyers = []
 
 let product 
 
 function getProductById(){
     if (productId){
+        console.log('productId', productId)
         let url =  `${API_URL}/product/${productId}`
         let xhr = new XMLHttpRequest();
         xhr.open('GET', url, true);
@@ -45,8 +45,28 @@ function getCobuyersOfAProduct(){
         xhr.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
         xhr.onreadystatechange = function (e){
             if (xhr.readyState == 4 && xhr.status == 200){
-                cobuyers = JSON.parse(xhr.responseText);
+                let cobuyers = JSON.parse(xhr.responseText);
                 console.log('cobuyers', cobuyers)
+                for (let i = 0; i < cobuyers.length; i++){
+                    const row = document.createElement('div')
+                    row.style.display = 'flex'
+                    row.style.alignItems = 'center'
+                    const newcoAvatar = document.createElement('div')
+                    newcoAvatar.style.width = '50px'
+                    newcoAvatar.style.height = '50px'
+                    newcoAvatar.style.borderRadius = '50%'
+                    newcoAvatar.style.backgroundColor = 'silver'
+                    newcoAvatar.style.display = 'flex'
+                    newcoAvatar.style.justifyContent = 'center'
+                    newcoAvatar.style.alignItems = 'center'
+                    newcoAvatar.style.marginRight = '10px'
+                    newcoAvatar.innerHTML = cobuyers[i].firstName[0] + cobuyers[i].lastName[0]
+                    const newcoName = document.createElement('div')
+                    newcoName.innerHTML = cobuyers[i].firstName + " " + cobuyers[i].lastName
+                    row.appendChild(newcoAvatar)
+                    row.appendChild(newcoName)
+                    cobuyerContainer.appendChild(row)
+                }
             }
         
         }
